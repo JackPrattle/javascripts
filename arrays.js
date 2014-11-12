20a2ce4a454ef51cd60c425a96f313eb87be1f76
jackprattle <jackprattle@163.com>
2014-11-12 12:40:45 +0800

    test

---
+++ arrays.js
@@ -0,0 +1,4 @@
+var pizzaToppings =['tomato sauce','cheese','pepperoni'];
+
+//for (var w in pizzaToppings)
+console.log(pizzaToppings);
---
+++ looping-through-arrays.js
@@ -0,0 +1,5 @@
+var pets=['cat','dog','rat'];
+for (var w in pets)
+pets[w]=pets[w]+'s';
+
+console.log(pets);
